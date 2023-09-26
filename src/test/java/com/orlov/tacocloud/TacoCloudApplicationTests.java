2538e32a694dfe2e195f2fe9bb9673f327659387
Орлов Денис <orlovdenis.m@yandex.ru>
2023-09-26 18:53:21 +0500

    init

---
+++ src/test/java/com/orlov/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlov.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
